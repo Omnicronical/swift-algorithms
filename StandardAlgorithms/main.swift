7c88932e86a86b99d2a081d1daf02b80b120e546
Oscar Hassall <hassall.o@etoncollege.org.uk>
2019-11-05 12:18:07 +0000

    Initial commit of project files

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Hassall, Oscar (HWTA) on 05/11/2019.
+//  Copyright © 2019 Hassall, Oscar (HWTA). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let name = "Oscar"
+
+for i in 1...3 {
+    print(i)
+    print(name)
+}
